6b85118abb7d6e545c5aabc3bf0454a3b85234c1
VAPD <0202414037@uns.edu.pe>
2026-05-11 16:14:21 -0500

    v6:

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-history
 console.log("Hola mundo");
 console.log("eres un genio");
 console.log("soy cvastian");
 console.log("bro eres un genio");
 console.log("que tal amigos?");
+console.log("Aprendiendo Git");
